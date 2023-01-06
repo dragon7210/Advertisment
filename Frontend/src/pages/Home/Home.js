582e863cae6311e2d825f1dbe4fa5fd4e9312197
dragon <isanoguchi721@gmail.com>
2023-01-06 06:22:37 -0500

    fix

--- Frontend/src/pages/Home/Home.js
+++ Frontend/src/pages/Home/Home.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import Input from "../../components/Input";
 
 const Home = () => {
+  const [searchWord, setSearchWord] = useState("");
+  const onChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+  console.log(searchWord);
   return (
     <div>
-      <div className="w-[400px] mx-auto mt-[10vh]">
-        <Input placeholder="Enter the search keyword" />
+      <div className="w-[400px] mx-auto mt-[10vh] flex">
+        <Input
+          placeholder="Enter the search keyword"
+          type="text"
+          onChange={onChange}
+        />
+        <button className="bg-[#c7f2ff] px-5 rounded-[10px] mt-8 ml-5 hover:bg-[blue]">
+          <p className="text-black font-bold hover:text-white">Search</p>
+        </button>
       </div>
     </div>
   );
