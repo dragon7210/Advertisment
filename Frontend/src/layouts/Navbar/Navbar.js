4305103845e5631915b8cc52303c314d5d79220f
dragon <isanoguchi721@gmail.com>
2023-01-07 06:31:40 -0500

    fix

--- Frontend/src/layouts/Navbar/Navbar.js
+++ Frontend/src/layouts/Navbar/Navbar.js
@@ -8,32 +8,32 @@ export default function Navbar() {
     <nav className="h-[50px] bg-[#c7f2ff] px-[2%] text-white flex justify-end">
       <div className="my-auto flex">
         <ul className="flex ml-[20px]">
-          <li
+          <button
             className="my-auto mr-5 p-[5px] text-black cursor-pointer font-bold hover:bg-[#6dabe4] hover:rounded-lg hover:text-[white]"
             onClick={() => navigate("/home")}
           >
             Home
-          </li>
-          <li
+          </button>
+          <button
             className="my-auto mr-5 p-[5px] text-black cursor-pointer font-bold hover:bg-[#6dabe4] hover:rounded-lg hover:text-[white]"
             onClick={() => navigate("/post")}
           >
             New Post
-          </li>
-          <li
+          </button>
+          <button
             className="my-auto mr-5 p-[5px] text-black cursor-pointer font-bold hover:bg-[#6dabe4] hover:rounded-lg hover:text-[white]"
             onClick={() => navigate("/myposts")}
           >
             My Posts
-          </li>
+          </button>
           <button
             className="my-auto  ml-5 px-4 py-2 rounded-[10px] bg-[#6dabe4] hover:bg-[blue]"
             onClick={() => {
               localStorage.setItem("token", "");
               // window.location.href = "/";
               dispatch(clearIsLoggedIn);
-              window.location.href = '/';
-              navigate('/')
+              window.location.href = "/";
+              navigate("/");
             }}
           >
             LogOut
